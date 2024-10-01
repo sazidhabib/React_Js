f6f07a10fe261b3aad6ed7f7eeb764354390c8a9
Mahbub Sazid Habib <68610034+sazidhabib@users.noreply.github.com>
2024-10-01 19:57:07 +0600

    1 october update.

--- ReactBasics/my-app/src/Components/Chart/Chart.js
+++ ReactBasics/my-app/src/Components/Chart/Chart.js
@@ -4,14 +4,16 @@ import ChartBar from "./ChartBar";
 import "./Chart.css";
 
 const Chart = (props) => {
+  const datapointValues = props.dataCharts.map((dataChart) => dataChart.value);
+  const totalMaximum = Math.max(...datapointValues);
   return (
     <div className="chart">
       {props.dataCharts.map((dataChart) => (
         <ChartBar
           key={dataChart.label}
           value={dataChart.value}
-          maxValue={null}
-          label={dataPoint.lable}
+          maxValue={totalMaximum}
+          label={dataChart.label}
         />
       ))}
     </div>
--- ReactBasics/my-app/src/Components/Expense/Expenses.js
+++ ReactBasics/my-app/src/Components/Expense/Expenses.js
@@ -1,10 +1,9 @@
 import React, { useState } from "react";
-
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2022");
@@ -18,27 +17,30 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  let expensesContent = <p>No Expenses Found.</p>;
+  // let expensesContent = <p>No Expenses Found.</p>;
 
-  if (filteredExpense.length > 0) {
-    expensesContent = filteredExpense.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
-  }
+  // if (filteredExpense.length > 0) {
+  //   expensesContent = filteredExpense.map((expense) => (
+  //     <ExpenseItem
+  //       key={expense.id}
+  //       title={expense.title}
+  //       amount={expense.amount}
+  //       date={expense.date}
+  //     />
+  //   ));
+  // }
 
   return (
-    <Card className="expenses">
-      <ExpensesFilter
-        selected={filteredYear}
-        changeFilter={filterChangeHandler}
-      />
-      <ExpensesList items={filteredExpense} />
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          changeFilter={filterChangeHandler}
+        />
+        <ExpensesChart expenses={filteredExpense} />
+        <ExpensesList items={filteredExpense} />
+      </Card>
+    </div>
   );
 };
 export default Expenses;

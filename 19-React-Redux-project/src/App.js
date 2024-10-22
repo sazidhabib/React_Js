8de2586b4add2e8c1dfee191c7edc6396949661b
Mahbub Sazid Habib <68610034+sazidhabib@users.noreply.github.com>
2024-10-22 19:32:13 +0600

    22 october update.

--- 19-React-Redux-project/src/App.js
+++ 19-React-Redux-project/src/App.js
@@ -1,9 +1,23 @@
-import Counter from './components/Counter';
+import { Fragment } from "react";
+import { useSelector } from "react-redux";
 
+import Counter from "./components/Counter";
+import Header from "./components/Header";
+import Auth from "./components/Auth";
+import UserProfile from "./components/UserProfile";
+
+//import { authActions } from "../Store/index";
 
 function App() {
+  const isAuth = useSelector((state) => state.auth.isAuthenticated);
+
   return (
-    <Counter />
+    <Fragment>
+      <Header />
+      {!isAuth && <Auth />}
+      {isAuth && <UserProfile />}
+      <Counter />
+    </Fragment>
   );
 }
 
--- 19-React-Redux-project/src/Store/index.js
+++ 19-React-Redux-project/src/Store/index.js
@@ -1,31 +1,13 @@
-import { createSlice, configureStore } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 
-const initialState = { counter: 0, showCounter: true };
-
-const counterSlice = createSlice({
-  name: "counter",
-  initialState, //initialState: initialState
-  reducers: {
-    increment(state) {
-      state.counter++;
-    },
-    decreament(state) {
-      state.counter--;
-    },
-    increse(state, action) {
-      state.counter = state.counter + action.payload;
-    },
-    toggleCounter(state) {
-      state.showCounter = !state.showCounter;
-    },
-  },
-});
+import counterReducer from "./counter";
+import authReducer from "./auth";
 
 const store = configureStore({
-  reducer: counterSlice.reducer,
+  reducer: { counter: counterReducer, auth: authReducer },
 });
 
-export const counterActions = counterSlice.actions;
+
 export default store;
 
 // const counterReducer = (state = initialState, action) => {

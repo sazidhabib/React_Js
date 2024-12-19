8cdde9359ae82b5767753f031997fea2b1743576
Mahbub Sazid Habib <68610034+sazidhabib@users.noreply.github.com>
2024-12-19 23:52:20 +0600

    react Project done.

--- softnio company task/ShopingCard/shopingcard/src/App.jsx
+++ softnio company task/ShopingCard/shopingcard/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import CartModal from "./Component/CartModal copy";
 import ShopCard from "./Component/ShopCard";
 
 function App() {
   return (
     <>
       <ShopCard />
+      {/* <CartModal /> */}
     </>
   );
 }
--- softnio company task/ShopingCard/shopingcard/src/Component/RatingBar copy.jsx
+++ softnio company task/ShopingCard/shopingcard/src/Component/RatingBar copy.jsx
@@ -1,6 +1,7 @@
+import React from "react";
 import { FaStar, FaStarHalfAlt, FaRegStar } from "react-icons/fa";
 
-const RatingBar = ({ rating }) => {
+const RatingBar = ({ rating, reviews }) => {
   const stars = [];
 
   for (let i = 1; i <= 5; i++) {
@@ -21,8 +22,24 @@ const RatingBar = ({ rating }) => {
       <div className=" gap-2" style={{ display: "flex" }}>
         {stars}
       </div>
+      <span style={{ color: "#6c757d" }}>({reviews} Reviews)</span>
     </div>
   );
 };
 
 export default RatingBar;
+
+// how to use it
+// import React from 'react';
+// import RatingBar from './Rating';
+
+// function App() {
+//   return (
+//     <div>
+//       <h2>Product Rating</h2>
+//       <RatingBar rating={3.5} reviews={2} />
+//     </div>
+//   );
+// }
+
+// export default App;

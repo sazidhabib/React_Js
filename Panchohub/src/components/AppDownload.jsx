925adc1c1f15aec9e12b27dbc411b7fe4b2bd69b
Mahbub Sazid Habib <68610034+sazidhabib@users.noreply.github.com>
2025-06-22 20:05:24 +0600

    last update.

--- Panchohub/src/components/AppDownload.jsx
+++ Panchohub/src/components/AppDownload.jsx
@@ -7,7 +7,7 @@ const AppDownload = () => {
             <header className="grid !min-h-[49rem] bg-blue-600 px-8">
                 <div className="container mx-auto mt-32 grid h-full w-full grid-cols-1 place-items-center lg:mt-14 lg:grid-cols-2">
                     {/* Text Content */}
-                    <div className="col-span-1">
+                    <div className="col-span-1 text-left">
                         <h1 className="mb-4 text-4xl font-bold text-white md:text-5xl lg:text-6xl">
                             Your Perfect <br /> Learning App
                         </h1>
@@ -23,7 +23,7 @@ const AppDownload = () => {
                                 className="flex items-center justify-center gap-3 rounded-lg bg-white px-6 py-3 text-gray-900 transition-colors hover:bg-gray-100"
                             >
                                 <img
-                                    src="/logos/logo-apple.png"
+                                    src="image/download-1.svg"
                                     alt="App Store"
                                     className="h-6 w-6"
                                 />
@@ -52,12 +52,12 @@ const AppDownload = () => {
             </header>
 
             {/* Info Card */}
-            <div className="mx-8 -mt-24 rounded-xl bg-white p-5 shadow-md lg:mx-16 md:p-14">
+            <div className="mx-8 -mt-24 rounded-xl bg-white p-5 shadow-md lg:mx-16 md:p-14 text-left">
                 <div>
                     <h3 className="mb-3 text-2xl font-semibold text-blue-gray-900 md:text-3xl">
                         Learning App
                     </h3>
-                    <p className="font-normal text-gray-500 lg:w-5/12">
+                    <p className="font-normal text-gray-500">
                         Download our app to dive into a vast library of courses, tutorials,
                         and study materials on a wide range of subjects - from programming
                         and language learning to personal development and beyond

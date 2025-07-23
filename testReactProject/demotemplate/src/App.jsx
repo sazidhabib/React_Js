ced68e4f69d374e745c48e7f8e7029fdaa3700aa
Mahbub Sazid Habib <68610034+sazidhabib@users.noreply.github.com>
2025-07-23 20:05:11 +0600

    testimonial update

---
+++ testReactProject/demotemplate/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Testimonials from "./component/Testimonials";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-blue-500">Hello world!</h1>
+      <Testimonials />
+    </>
+  );
+}
+
+export default App;

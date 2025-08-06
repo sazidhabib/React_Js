85b034ebe6a5629994ea17966f1de16ce2110960
Mahbub Sazid Habib <68610034+sazidhabib@users.noreply.github.com>
2025-08-06 20:01:32 +0600

    connect mysql database.

--- Mysql_setup_for_React/Server/server.js
+++ Mysql_setup_for_React/Server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mysql = require('mysql2/promise');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
+const helmet = require('helmet');
 const app = express();
 
 // Security middleware
